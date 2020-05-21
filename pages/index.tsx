6a66c3fdc294a0284eeabcb6d7559b0e348d1185
Essential Randomness <essentialrandomn3ss@gmail.com>
2020-05-21 16:23:27 -0700

    enable uploading to gCloud

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,61 @@ import {
 import Head from "next/head";
 
 function HomePage() {
+  const [isPostLoading, setPostLoading] = React.useState(false);
+  const [posts, setPosts] = React.useState<any[]>([
+    {
+      createdTime: "5 minutes ago",
+      text:
+        '[{"insert":"Nishin Masumi Reading Group (Week 2)"},{"attributes":{"header":1},"insert":"\\n"},{"insert":"\\nAs you know, we\'re going through \\"Host is Down\\" this week! \\n\\n"},{"attributes":{"alt":"Host is Down by Mado Fuchiya (Nishin)"},"insert":{"image":"https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1564868627l/50190748._SX1200_SY630_.jpg"}},{"insert":"\\n\\nThis is the official discussion thread. Feel free to comment, but remember to tag spoilers (or suffer the mods\' wrath).\\n"}]',
+      secretIdentity: {
+        name: "Good Guy",
+        avatar: `https://placekitten.com/200/300`,
+      },
+      newPost: true,
+    },
+    {
+      createdTime: "10 hours ago",
+      text:
+        '[{"insert":"Help a Thirsty, Thirsty Anon"},{"attributes":{"header":1},"insert":"\\n"},{"insert":"\\nI recently discovered "},{"attributes":{"link":"https://myanimelist.net/manga/115345/MADK"},"insert":"MadK"},{"insert":", and I\'ve fallen in love with the combination of beautiful art and great story. I\'ve been trying to put together a list of recs of the angstiest, goriest series out there. It\'s been surprisingly hard to find the Good Shit.\\n\\nWhat\'s your favorite series and why?\\n"}]',
+      secretIdentity: {
+        name: "Tuxedo Mask",
+        avatar: `https://placekitten.com/200/300`,
+      },
+      userIdentity: {
+        name: "SexyDaddy69",
+        avatar: `https://placekitten.com/200/300`,
+      },
+      options: {
+        wide: true,
+      },
+      newComments: 5,
+    },
+    {
+      createdTime: "yesterday",
+      text:
+        '[{"insert":"Monthly Art Roundup"},{"attributes":{"header":1},"insert":"\\n"},{"insert":"\\nPost your favorites! As usual, remember to embed the actual posts (unless it\'s your own art, then do as you wish). Reposting is a no-no. \\n\\nI\'ll start with one of my favorite artists:\\n"},{"insert":{"tweet":"https://twitter.com/notkrad/status/1222638147886034945"}}]',
+      secretIdentity: {
+        name: "Bad Guy",
+        avatar: `https://placekitten.com/200/300`,
+      },
+      newComments: 5,
+      newContributions: 2,
+    },
+    {
+      createdTime: "3 days ago",
+      text:
+        '[{"insert":{"block-image":"https://media.tenor.com/images/97b761adf7bdc9d72fc1fadbbaa3a4a6/tenor.gif"}},{"insert":"(I got inspired to write a quick cannibalism drabble. Wanted to share it and get your opinion while I decide whether to turn it into a longer fic!)\\n"}]',
+      secretIdentity: {
+        name: "Nice Therapist",
+        avatar: `https://placekitten.com/200/300`,
+      },
+      userIdentity: {
+        name: "xXxChesapeakeRipperxXx",
+        avatar: `https://placekitten.com/200/300`,
+      },
+      newContributions: 3,
+    },
+  ]);
   const [showSidebar, setShowSidebar] = React.useState(false);
   const [postEditorOpen, setPostEditorOpen] = React.useState(false);
 
@@ -38,67 +93,41 @@ function HomePage() {
             name: "SexyDaddy69",
             avatar: `https://placekitten.com/200/300`,
           }}
-          onSubmit={(text) => console.log(text)}
+          loading={isPostLoading}
+          onSubmit={({ text, large }) => {
+            console.log(text);
+            setTimeout(() => {
+              setPosts([
+                {
+                  createdTime: "1 minute ago",
+                  text,
+                  secretIdentity: {
+                    name: "Tuxedo Mask",
+                    avatar: `https://placekitten.com/200/300`,
+                  },
+                  userIdentity: {
+                    name: "SexyDaddy69",
+                    avatar: `https://placekitten.com/200/300`,
+                  },
+                  options: {
+                    wide: large,
+                  },
+                  newPost: true,
+                },
+                ...posts,
+              ]);
+              setPostLoading(false);
+              setPostEditorOpen(false);
+            }, 3000);
+            setPostLoading(true);
+          }}
           onCancel={() => setPostEditorOpen(false)}
         />
       </Modal>
       <Layout
         mainContent={
           <BoardFeed
-            posts={[
-              {
-                createdTime: "5 minutes ago",
-                text:
-                  '[{"insert":"Nishin Masumi Reading Group (Week 2)"},{"attributes":{"header":1},"insert":"\\n"},{"insert":"\\nAs you know, we\'re going through \\"Host is Down\\" this week! \\n\\n"},{"attributes":{"alt":"Host is Down by Mado Fuchiya (Nishin)"},"insert":{"image":"https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1564868627l/50190748._SX1200_SY630_.jpg"}},{"insert":"\\n\\nThis is the official discussion thread. Feel free to comment, but remember to tag spoilers (or suffer the mods\' wrath).\\n"}]',
-                secretIdentity: {
-                  name: "Good Guy",
-                  avatar: `https://placekitten.com/200/300`,
-                },
-                newPost: true,
-              },
-              {
-                createdTime: "10 hours ago",
-                text:
-                  '[{"insert":"Help a Thirsty, Thirsty Anon"},{"attributes":{"header":1},"insert":"\\n"},{"insert":"\\nI recently discovered "},{"attributes":{"link":"https://myanimelist.net/manga/115345/MADK"},"insert":"MadK"},{"insert":", and I\'ve fallen in love with the combination of beautiful art and great story. I\'ve been trying to put together a list of recs of the angstiest, goriest series out there. It\'s been surprisingly hard to find the Good Shit.\\n\\nWhat\'s your favorite series and why?\\n"}]',
-                secretIdentity: {
-                  name: "Tuxedo Mask",
-                  avatar: `https://placekitten.com/200/300`,
-                },
-                userIdentity: {
-                  name: "SexyDaddy69",
-                  avatar: `https://placekitten.com/200/300`,
-                },
-                options: {
-                  wide: true,
-                },
-                newComments: 5,
-              },
-              {
-                createdTime: "yesterday",
-                text:
-                  '[{"insert":"Monthly Art Roundup"},{"attributes":{"header":1},"insert":"\\n"},{"insert":"\\nPost your favorites! As usual, remember to embed the actual posts (unless it\'s your own art, then do as you wish). Reposting is a no-no. \\n\\nI\'ll start with one of my favorite artists:\\n"},{"insert":{"tweet":"https://twitter.com/notkrad/status/1222638147886034945"}}]',
-                secretIdentity: {
-                  name: "Bad Guy",
-                  avatar: `https://placekitten.com/200/300`,
-                },
-                newComments: 5,
-                newContributions: 2,
-              },
-              {
-                createdTime: "3 days ago",
-                text:
-                  '[{"insert":{"block-image":"https://media.tenor.com/images/97b761adf7bdc9d72fc1fadbbaa3a4a6/tenor.gif"}},{"insert":"(I got inspired to write a quick cannibalism drabble. Wanted to share it and get your opinion while I decide whether to turn it into a longer fic!)\\n"}]',
-                secretIdentity: {
-                  name: "Nice Therapist",
-                  avatar: `https://placekitten.com/200/300`,
-                },
-                userIdentity: {
-                  name: "xXxChesapeakeRipperxXx",
-                  avatar: `https://placekitten.com/200/300`,
-                },
-                newContributions: 3,
-              },
-            ]}
+            posts={posts}
             showSidebar={showSidebar}
             onCloseSidebar={() => setShowSidebar(false)}
             boardInfo={{
