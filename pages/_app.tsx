dcd091ff75ca12010cf424feefe4953226abf5fd
Essential Randomness <essential.randomn3ss@gmail.com>
2021-03-05 13:20:04 -0800

    add svg favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -195,19 +195,20 @@ function MyApp({
           content={getDescription(currentBoardData)}
         />
         <meta name="twitter:image" content={getImage(currentBoardData)} />
+        <link rel="icon" type="image/svg+xml" href="/icons/logo-compact.svg" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
           href="/icons/apple-touch-icon.png"
         />
         <link
-          rel="icon"
+          rel="alternate icon"
           type="image/png"
           sizes="32x32"
           href="/icons/favicon-32x32.png"
         />
         <link
-          rel="icon"
+          rel="alternate icon"
           type="image/png"
           sizes="16x16"
           href="/icons/favicon-16x16.png"
