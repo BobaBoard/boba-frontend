1511833c1cc8faf712fab4a49f23ec03b0f725ec
Essential Randomness <essential.randomn3ss@gmail.com>
2021-05-19 18:51:54 -0700

    remove exif data from jpg/tiff files upon upload

--- utils/image-upload.tsx
+++ utils/image-upload.tsx
@@ -5,10 +5,47 @@ import noop from "noop-ts";
 
 import { getPageDetails } from "./router-utils";
 import { NextRouter } from "next/router";
+import loadImage from "blueimp-load-image";
 import debug from "debug";
 const error = debug("bobafrontend:postEditor-error");
 
-const createImageUploadPromise = ({
+const uploadImage = ({
+  baseUrl,
+  extension,
+  imageData,
+}: {
+  baseUrl: string;
+  extension: string;
+  imageData: string;
+}): Promise<string> => {
+  const ref = firebase.storage().ref(baseUrl).child(`${uuidv4()}${extension}`);
+
+  return new Promise((onSuccess, onReject) => {
+    ref
+      .putString(imageData, "data_url")
+      .on(firebase.storage.TaskEvent.STATE_CHANGED, {
+        complete: () => {
+          ref.getDownloadURL().then((url) => onSuccess(url));
+        },
+        next: noop,
+        error: (e) => {
+          error(e);
+          onReject(e);
+        },
+      });
+  });
+};
+
+const applyAndStripExif = async (imageData: string) => {
+  const imageWithRotation = await loadImage(imageData, {
+    orientation: true,
+    canvas: true,
+  });
+  return (imageWithRotation.image as any as HTMLCanvasElement).toDataURL();
+};
+
+const STRIP_EXIF_EXTENSIONS = ["jpeg", "jpg", "tiff"];
+const createImageUploadPromise = async ({
   imageData,
   slug,
   threadId,
@@ -17,41 +54,29 @@ const createImageUploadPromise = ({
   slug: string | null;
   threadId: string | null;
 }) => {
-  return new Promise<string>((onSuccess, onReject) => {
-    // Do not upload tenor stuff
-    if (imageData.startsWith("https://media.tenor.com/")) {
-      onSuccess(imageData);
-      return;
+  // Do not upload tenor stuff
+  if (imageData.startsWith("https://media.tenor.com/")) {
+    return imageData;
+  }
+  const baseUrl = `images/${slug}/${threadId ? threadId + "/" : ""}`;
+  // Upload base 64 images
+  if (imageData.startsWith("data:image")) {
+    let dataType = imageData.match(/[^:/]\w+(?=;|,)/)?.[0];
+    let strippedData = imageData;
+    if (!dataType || STRIP_EXIF_EXTENSIONS.includes(dataType.toLowerCase())) {
+      strippedData = await applyAndStripExif(imageData);
+      dataType = strippedData.match(/[^:/]\w+(?=;|,)/)?.[0];
     }
-    const baseUrl = `images/${slug}/${threadId ? threadId + "/" : ""}`;
-    // Upload base 64 images
-    if (imageData.startsWith("data:image")) {
-      const dataType = imageData.match(/[^:/]\w+(?=;|,)/)?.[0];
-      const extension = dataType ? `.${dataType}` : "";
-      const ref = firebase
-        .storage()
-        .ref(baseUrl)
-        .child(`${uuidv4()}${extension}`);
+    const extension = dataType ? `.${dataType}` : "";
 
-      ref
-        .putString(imageData, "data_url")
-        .on(firebase.storage.TaskEvent.STATE_CHANGED, {
-          complete: () => {
-            ref.getDownloadURL().then((url) => onSuccess(url));
-          },
-          next: noop,
-          error: (e) => {
-            error(e);
-            onReject(e);
-          },
-        });
-      return;
-    }
-    // else, for now, let's just swap it with the Onceler.
-    onSuccess(
-      "https://firebasestorage.googleapis.com/v0/b/bobaboard-fb.appspot.com/o/images%2Fmain_street%2Fe7533caf-c4e4-42c5-a668-21236995156a.jpeg?alt=media&token=c9804eb5-f72e-46a2-8854-0a7b154a7ecd"
-    );
-  });
+    return await uploadImage({
+      baseUrl,
+      extension,
+      imageData: strippedData,
+    });
+  }
+  // else, for now, let's just swap it with the Onceler.
+  return "https://firebasestorage.googleapis.com/v0/b/bobaboard-fb.appspot.com/o/images%2Fmain_street%2Fe7533caf-c4e4-42c5-a668-21236995156a.jpeg?alt=media&token=c9804eb5-f72e-46a2-8854-0a7b154a7ecd";
 };
 
 export const useImageUploader = (router: NextRouter) => {
