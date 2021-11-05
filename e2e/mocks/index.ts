3bbd0aaf5698140779be8b37bf728dbc80aad592
Essential Randomness <essential.randomn3ss@gmail.com>
2021-11-05 12:49:28 -0700

    Revert "generate mock server given backend examples"
    This reverts commit 9d191a0483d1dfd41dfd90863e01bc6b975e30fe.

--- e2e/mocks/index.ts
+++ e2e/mocks/index.ts
@@ -1,12 +1,12 @@
-import handlers from "./generated";
+import boards from "./boards";
 import { setupServer } from "msw/node";
 import { setupWorker } from "msw";
 
 if (typeof window !== "undefined") {
-  const worker = setupWorker(...handlers);
+  const worker = setupWorker(...boards);
   worker.start();
 } else {
-  const server = setupServer(...handlers);
+  const server = setupServer(...boards);
   server.listen();
 }
 export default {};
