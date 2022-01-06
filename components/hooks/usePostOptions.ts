86c42e15dc6ef98f66b95cd30cf4757b461d2250
Essential Randomness <essential.randomn3ss@gmail.com>
2022-01-05 21:23:24 -0800

    test thread link copy option

--- components/hooks/usePostOptions.ts
+++ components/hooks/usePostOptions.ts
@@ -54,6 +54,7 @@ const copyText = (text: string) => {
   const tempInput = document.createElement("input");
   tempInput.value = text;
   document.body.appendChild(tempInput);
+  tempInput.focus();
   tempInput.select();
   document.execCommand("copy");
   document.body.removeChild(tempInput);
