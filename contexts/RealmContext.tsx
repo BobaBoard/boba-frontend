706e0a1410e35c22ffdb5ef3812d129f14ee3a0a
VirtuousCat <91882409+virtuous-cat@users.noreply.github.com>
2022-04-27 01:47:52 -0700

    add realm id

--- contexts/RealmContext.tsx
+++ contexts/RealmContext.tsx
@@ -49,6 +49,14 @@ const useRealmSettings = () => {
   return context.realmData.settings;
 };
 
+const useRealmId = () => {
+  const context = React.useContext(RealmContext);
+  if (context === undefined) {
+    throw new Error("useRealmId must be used within a RealmContextProvider");
+  }
+  return context.realmData.id;
+};
+
 const useRealmPermissions = () => {
   const context = React.useContext(RealmContext);
   if (context === undefined) {
@@ -163,6 +171,7 @@ export {
   MemoizedProvider as RealmContextProvider,
   useRealmContext,
   useRealmSettings,
+  useRealmId,
   useRealmPermissions,
   useRealmBoards,
   useRealmHomepage,
