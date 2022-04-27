1d787f7d03da72dd25dea00153c1c92e5834a944
VirtuousCat <91882409+virtuous-cat@users.noreply.github.com>
2022-04-27 01:48:58 -0700

    Add invite queries

--- utils/queries/realm.ts
+++ utils/queries/realm.ts
@@ -1,6 +1,10 @@
 import { RealmType } from "types/Types";
 import axios from "axios";
+import debug from "debug";
 import { makeRealmData } from "utils/client-data";
+import { useRealmId } from "contexts/RealmContext";
+
+const log = debug("bobafrontend:queries:realm-log");
 
 export const getRealmData = async ({
   realmSlug,
@@ -10,4 +14,28 @@ export const getRealmData = async ({
   const response = await axios.get(`/realms/slug/${realmSlug}`);
 
   return makeRealmData(response.data);
+};
+
+export const getRealmInvites = async (realmId: string): Promise<any> => {
+  const response = await axios.get(`/realms/${realmId}/invites`);
+  log(`Got realm invites from server:`);
+  log(response.data);
+  return response.data;
+};
+
+export const createInvite = async (
+  realmId: string,
+  email: string,
+  label?: unknown
+): Promise<any> => {
+  const response = await axios.patch(`/realms/${realmId}/settings`, {
+    email,
+    label,
+  });
+  if (response.status !== 200) {
+    throw new Error("Error while creating invite.");
+  }
+  log(`created invite on the server:`);
+  log(response?.data);
+  return response?.data;
 };
