f1bd6c17f11ecf750cb1ee92447626c47cff667a
VirtuousCat <91882409+virtuous-cat@users.noreply.github.com>
2022-05-03 02:48:10 -0700

    Update invite tests

--- tests/UI/InvitesPanel.test.tsx
+++ tests/UI/InvitesPanel.test.tsx
@@ -1,25 +1,21 @@
-import AdminPage, { AdminPanelIds } from "pages/realms/admin/[[...panelId]]";
 import { Client, getAdminPanelRoute } from "./utils";
 import {
   LOGGED_IN_V0_DATA,
   V0_CREATED_INVITE,
   V0_INVITES,
 } from "../server-mocks/data/realm";
-import {
-  fireEvent,
-  prettyDOM,
-  render,
-  screen,
-  waitFor,
-  within,
-} from "@testing-library/react";
+import { render, screen, waitFor, within } from "@testing-library/react";
 
+import AdminPage from "pages/realms/admin/[[...panelId]]";
 import React from "react";
+import debug from "debug";
 import { format } from "date-fns";
 import { makeRealmData } from "utils/client-data";
 import { rest } from "msw";
-import { server } from "tests/server-mocks";
+import { server } from "../server-mocks";
 import userEvent from "@testing-library/user-event";
+
+const log = debug("bobafrontend:tests:UI:InvitesPanel");
 
 const ADMIN_ROUTER = getAdminPanelRoute({
   adminPanel: "invite-form",
@@ -51,7 +47,11 @@ describe("InvitesPanel", () => {
       screen.getByRole("form", { name: "Create Realm Invite" })
     ).toBeVisible();
     expect(screen.getByLabelText("Email:")).toBeVisible();
-    expect(screen.getByLabelText("Label:")).toBeVisible();
+    expect(
+      screen.getByLabelText(
+        "Label (optional - all Realm admins will be able to see this):"
+      )
+    ).toBeVisible();
     expect(screen.getByRole("button", { name: "Create Invite" })).toBeVisible();
   });
 
@@ -75,7 +75,7 @@ describe("InvitesPanel", () => {
       expect(table).toBeVisible();
       expect(
         screen.queryByRole("list", { name: "Pending Realm Invites" })
-      ).not.toBeVisible();
+      ).not.toBeInTheDocument();
 
       const invites = table.getElementsByTagName("tr");
       expect(invites).toHaveLength(V0_INVITES.invites.length + 1);
@@ -89,23 +89,29 @@ describe("InvitesPanel", () => {
         } else {
           expect(
             within(row).getByText(
-              format(new Date(V0_INVITES.invites[i].issued_at), "MMM d, yyyy")
+              format(
+                new Date(V0_INVITES.invites[i - 1].issued_at),
+                "MMM d, yyyy"
+              )
             )
           ).toBeVisible();
           expect(
             within(row).getByText(
-              format(new Date(V0_INVITES.invites[i].expires_at), "MMM d, yyyy")
+              format(
+                new Date(V0_INVITES.invites[i - 1].expires_at),
+                "MMM d, yyyy"
+              )
             )
           ).toBeVisible();
           expect(
-            within(row).getByText(V0_INVITES.invites[i].invite_url)
+            within(row).getByText(V0_INVITES.invites[i - 1].invite_url)
           ).toBeVisible();
-          if (V0_INVITES.invites[i].label) {
+          if (V0_INVITES.invites[i - 1].label) {
             expect(
-              within(row).getByText(V0_INVITES.invites[i].label!)
+              within(row).getByText(V0_INVITES.invites[i - 1].label!)
             ).toBeVisible();
           }
-          V0_INVITES.invites[i].own
+          V0_INVITES.invites[i - 1].own
             ? expect(within(row).getByText("You")).toBeVisible()
             : expect(within(row).getByText("Another Admin")).toBeVisible();
         }
@@ -113,65 +119,84 @@ describe("InvitesPanel", () => {
     });
   });
 
-  // test("renders pending realm invites list as list when screen is narrow", async () => {
-  //   render(
-  //     <Client
-  //       router={ADMIN_ROUTER}
-  //       initialData={{ realm: makeRealmData(LOGGED_IN_V0_DATA) }}
-  //     >
-  //       <AdminPage />
-  //     </Client>
-  //   );
+  test("renders pending realm invites list as list when screen is narrow", async () => {
+    // I feel like there has to be a more elegant way to make the media match true for this one test
+    // than setting this whole thing again at the beginning and setting it back to false at the end, but I couldn't figure it out
+    Object.defineProperty(window, "matchMedia", {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: true,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
 
-  //   const headings = screen.getAllByRole("heading", { level: 2 });
-  //   expect(headings).toHaveLength(2);
-  //   expect(headings[1]).toHaveTextContent("Pending Realm Invites");
-  //   await waitFor(() => {
-  //     const invitesList = screen.getByRole("list", {
-  //       name: "Pending Realm Invites",
-  //     });
-  //     expect(invitesList).toBeVisible();
-  //     expect(
-  //       screen.queryByRole("table", { name: "Pending Realm Invites" })
-  //     ).not.toBeVisible();
+    render(
+      <Client
+        router={ADMIN_ROUTER}
+        initialData={{ realm: makeRealmData(LOGGED_IN_V0_DATA) }}
+      >
+        <AdminPage />
+      </Client>
+    );
 
-  //     const invites = within(invitesList).getAllByRole("list");
-  //     expect(invites).toHaveLength(V0_INVITES.invites.length);
-  //     invites.forEach((invite, i) => {
-  //       expect(
-  //         within(invite).getByText(
-  //           `Created: ${format(
-  //             new Date(V0_INVITES.invites[i].issued_at),
-  //             "MMM d, yyyy"
-  //           )}`
-  //         )
-  //       ).toBeVisible();
-  //       expect(
-  //         within(invite).getByText(
-  //           `Expires: ${format(
-  //             new Date(V0_INVITES.invites[i].expires_at),
-  //             "MMM d, yyyy"
-  //           )}`
-  //         )
-  //       ).toBeVisible();
-  //       expect(
-  //         within(invite).getByText(
-  //           `Invite URL: ${V0_INVITES.invites[i].invite_url}`
-  //         )
-  //       ).toBeVisible();
-  //       if (V0_INVITES.invites[i].label) {
-  //         expect(
-  //           within(invite).getByText(`Label: ${V0_INVITES.invites[i].label!}`)
-  //         ).toBeVisible();
-  //       }
-  //       V0_INVITES.invites[i].own
-  //         ? expect(within(invite).getByText("Created By: You")).toBeVisible()
-  //         : expect(
-  //             within(invite).getByText("Created By: Another Admin")
-  //           ).toBeVisible();
-  //     });
-  //   });
-  // });
+    const headings = screen.getAllByRole("heading", { level: 2 });
+    expect(headings).toHaveLength(2);
+    expect(headings[1]).toHaveTextContent("Pending Realm Invites");
+    await waitFor(() => {
+      const invitesList = screen.getByRole("list", {
+        name: "Pending Realm Invites",
+      });
+      expect(invitesList).toBeVisible();
+      expect(
+        screen.queryByRole("table", { name: "Pending Realm Invites" })
+      ).not.toBeInTheDocument();
+
+      const invites = within(invitesList).getAllByRole("list");
+      expect(invites).toHaveLength(V0_INVITES.invites.length);
+      invites.forEach((invite, i) => {
+        expect(
+          within(invite).getByText(
+            format(new Date(V0_INVITES.invites[i].issued_at), "MMM d, yyyy")
+          )
+        ).toBeVisible();
+        expect(
+          within(invite).getByText(
+            format(new Date(V0_INVITES.invites[i].expires_at), "MMM d, yyyy")
+          )
+        ).toBeVisible();
+        expect(
+          within(invite).getByText(V0_INVITES.invites[i].invite_url)
+        ).toBeVisible();
+        if (V0_INVITES.invites[i].label) {
+          expect(
+            within(invite).getByText(V0_INVITES.invites[i].label!)
+          ).toBeVisible();
+        }
+        V0_INVITES.invites[i].own
+          ? expect(within(invite).getByText("You")).toBeVisible()
+          : expect(within(invite).getByText("Another Admin")).toBeVisible();
+      });
+    });
+    Object.defineProperty(window, "matchMedia", {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
+  });
 
   test("doesn't render pending realm invites list if empty", async () => {
     server.use(
@@ -191,10 +216,10 @@ describe("InvitesPanel", () => {
     await waitFor(() => {
       expect(
         screen.queryByRole("table", { name: "Pending Realm Invites" })
-      ).not.toBeVisible();
+      ).not.toBeInTheDocument();
       expect(
         screen.queryByRole("list", { name: "Pending Realm Invites" })
-      ).not.toBeVisible();
+      ).not.toBeInTheDocument();
       expect(
         screen.getByText("There are no currently pending invites.")
       ).toBeVisible();
@@ -219,10 +244,17 @@ describe("InvitesPanel", () => {
       V0_CREATED_INVITE.invitee_email
     );
 
-    userEvent.type(screen.getByLabelText("Label:"), V0_CREATED_INVITE.label);
-    expect(screen.getByLabelText("Label:")).toHaveValue(
+    userEvent.type(
+      screen.getByLabelText(
+        "Label (optional - all Realm admins will be able to see this):"
+      ),
       V0_CREATED_INVITE.label
     );
+    expect(
+      screen.getByLabelText(
+        "Label (optional - all Realm admins will be able to see this):"
+      )
+    ).toHaveValue(V0_CREATED_INVITE.label);
 
     userEvent.click(screen.getByRole("button", { name: "Create Invite" }));
     await waitFor(() => {
@@ -234,44 +266,18 @@ describe("InvitesPanel", () => {
           screen.getByRole("form", { name: "Create Realm Invite" })
         ).getByText(V0_CREATED_INVITE.invite_url)
       ).toBeVisible();
-      const expectedInvites = [...V0_INVITES.invites, V0_CREATED_INVITE];
-      const invites = screen
-        .getByRole("table", {
-          name: "Pending Realm Invites",
-        })
-        .getElementsByTagName("tr");
-      expect(invites).toHaveLength(expectedInvites.length + 1);
-      Array.from(invites).forEach((row, i) => {
-        if (i === 0) {
-          expect(within(row).getByText("Created")).toBeVisible();
-          expect(within(row).getByText("Expires")).toBeVisible();
-          expect(within(row).getByText("Invite URL")).toBeVisible();
-          expect(within(row).getByText("Label")).toBeVisible();
-          expect(within(row).getByText("Created By")).toBeVisible();
-        } else {
-          expect(
-            within(row).getByText(
-              format(new Date(expectedInvites[i].issued_at), "MMM d, yyyy")
-            )
-          ).toBeVisible();
-          expect(
-            within(row).getByText(
-              format(new Date(expectedInvites[i].expires_at), "MMM d, yyyy")
-            )
-          ).toBeVisible();
-          expect(
-            within(row).getByText(expectedInvites[i].invite_url)
-          ).toBeVisible();
-          if (expectedInvites[i].label) {
-            expect(
-              within(row).getByText(expectedInvites[i].label!)
-            ).toBeVisible();
-          }
-          expectedInvites[i].own
-            ? expect(within(row).getByText("You")).toBeVisible()
-            : expect(within(row).getByText("Another Admin")).toBeVisible();
-        }
-      });
+    });
+    await waitFor(() => {
+      expect(
+        within(
+          screen.getByRole("table", { name: "Pending Realm Invites" })
+        ).getByText(V0_CREATED_INVITE.label)
+      ).toBeVisible();
+      expect(
+        within(
+          screen.getByRole("table", { name: "Pending Realm Invites" })
+        ).getByText(V0_CREATED_INVITE.invite_url)
+      ).toBeVisible();
     });
   });
 });
