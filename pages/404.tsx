a322c1b72a8caa7c1d4ba8e642419ee473f254ce
Essential Randomness <essential.randomn3ss@gmail.com>
2022-06-03 12:34:06 -0700

    create a 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,25 @@
+import Layout from "components/layout/Layout";
+import React from "react";
+
+function NotFoundPage() {
+  return (
+    <Layout title={"Wooops!"}>
+      <Layout.MainContent>
+        <div className="main">
+          <img src="/error.png" />
+          <h2>The page you tried to reach was not found on this server!</h2>
+          <style jsx>{`
+            .main {
+              color: white;
+              width: 100%;
+              text-align: center;
+              padding: 50px 20px;
+            }
+          `}</style>
+        </div>
+      </Layout.MainContent>
+    </Layout>
+  );
+}
+
+export default NotFoundPage;
