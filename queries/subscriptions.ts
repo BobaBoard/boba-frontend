dcb692bf409e819169e0f58827485fdd13881023
Essential Randomness <essential.randomn3ss@gmail.com>
2022-06-08 14:25:34 -0700

    create utilities to fetch subscriptions

---
+++ queries/subscriptions.ts
@@ -0,0 +1,31 @@
+import { QueryClient, useQuery } from "react-query";
+
+import { SubscriptionFeed } from "types/Types";
+import { getLatestSubscriptionUpdate } from "utils/queries/subscription";
+
+const SUBSCRIPTION_KEY = "subscriptions";
+
+export const useSubscription = ({
+  subscriptionId,
+}: {
+  subscriptionId: string;
+}) => {
+  const { data: subscriptionData } = useQuery<SubscriptionFeed>(
+    [SUBSCRIPTION_KEY, { subscriptionId }],
+    () => getLatestSubscriptionUpdate({ subscriptionId }),
+    {
+      staleTime: Infinity,
+    }
+  );
+  return subscriptionData;
+};
+
+export const prefetchSubscriptionData = async (
+  queryClient: QueryClient,
+  { subscriptionId }: { subscriptionId: string }
+) => {
+  return await queryClient.setQueryData(
+    [SUBSCRIPTION_KEY, { subscriptionId }],
+    await getLatestSubscriptionUpdate({ subscriptionId })
+  );
+};
