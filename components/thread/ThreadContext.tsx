dfe0f97615d4e2d6dbd544075161cdc3077bd705
Essential Randomness <essential.randomn3ss@gmail.com>
2022-10-18 16:39:50 -0700

    remove console logs x 2

--- components/thread/ThreadContext.tsx
+++ components/thread/ThreadContext.tsx
@@ -170,7 +170,6 @@ export const useThreadMetadata = ({
     currentRoot =
       threadData.posts.find((post) => post.postId == comment?.parentPostId) ??
       null;
-    console.log(currentRoot);
   }
 
   return {
