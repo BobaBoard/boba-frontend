ffe721fac03f3278623e83f48661f0a9a3bdcd0b
Essential Randomness <essential.randomn3ss@gmail.com>
2023-10-25 17:09:21 -0700

    spurious changes

--- kubb.config.ts
+++ kubb.config.ts
@@ -4,7 +4,9 @@ import createSwaggerClient from "@kubb/swagger-client";
 import createSwaggerTS from "@kubb/swagger-ts";
 // import createSwaggerZod from "@kubb/swagger-zod";
 import { defineConfig } from "@kubb/core";
-// import openApiSpec from "@bobaboard/boba-backend-open-api";
+import openApiSpec from "@bobaboard/boba-backend-open-api";
+
+console.log(openApiSpec);
 
 export default defineConfig(async () => {
   return {
